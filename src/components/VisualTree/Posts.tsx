4946a5b177288a1a82e90ae5cbd72570a53d1748
Eduardo Magaldi <eduardo.magaldi@llac.adv.br>
2024-11-30 19:30:02 -0300

    another example of working suspense

---
+++ src/components/VisualTree/Posts.tsx
@@ -0,0 +1,58 @@
+let status = 'pending'
+let result: any
+
+const postsData = fetchPosts('https://fake-api.tractian.com/companies')
+
+// Posts component (definition)
+const Posts = () => {
+  // No need for loading states
+  const posts = postsData()
+  return (
+    <div>
+      {posts.map((post: any, index) => (
+        <div key={index}>{post.name}</div>
+      ))}
+    </div>
+  )
+}
+
+export default Posts
+
+// Fetch external data
+function fetchPosts(url: string) {
+  let fetching = fetch(url)
+    .then((res) => res.json())
+    .then(async (res) => {
+      await sleep(1000)
+      return res
+    })
+    .then((success) => {
+      console.log('success', success)
+
+      status = 'fulfilled'
+
+      result = success
+    })
+    // Fetch request has failed
+    .catch((error) => {
+      status = 'rejected'
+
+      result = error
+    })
+
+  console.log('result', result)
+
+  return () => {
+    if (status === 'pending') {
+      throw fetching // Suspend(A way to tell React data is still fetching)
+    } else if (status === 'rejected') {
+      throw result // Result is an error
+    } else if (status === 'fulfilled') {
+      return result // Result is a fulfilled promise
+    }
+  }
+}
+
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
--- src/components/VisualTree/index.tsx
+++ src/components/VisualTree/index.tsx
@@ -1,14 +1,48 @@
-import { Suspense } from 'react'
-import Todos from './Todos'
+// import { Suspense } from 'react'
+// import Todos from './Todos'
 
-const App = () => {
+import { Suspense } from 'react'
+import Posts from './Posts'
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Suspense fallback={<p>Loading Todos...</p>}>
+//         <Todos />
+//       </Suspense>
+//     </div>
+//   )
+// }
+
+// export default App
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+const MyPosts = () => {
   return (
-    <div>
-      <Suspense fallback={<p>Loading Todos...</p>}>
-        <Todos />
+    <div style={{ marginLeft: '20px' }}>
+      <h1>My Posts</h1>
+      {/* Wrap the <Posts /> component in React.Suspense to display a fallback(spinner)
+      while the component is not yet ready to render (data is still fetching) */}
+
+      <Suspense fallback={<>Loading...</>}>
+        <Posts />
       </Suspense>
     </div>
   )
 }
 
-export default App
+export default MyPosts
