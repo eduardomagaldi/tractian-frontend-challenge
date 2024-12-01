4a036f9bb8a0b548934c5b010b19a7d5dd71a01f
Eduardo Magaldi <eduardo.magaldi@llac.adv.br>
2024-12-01 18:44:18 -0300

    making app look good

--- src/components/CompaniesList/Posts.tsx
+++ src/components/CompaniesList/Posts.tsx
@@ -1,13 +1,20 @@
-const postsData = fetchPosts('https://fake-api.tractian.com/companies')
+import './style.css'
 
-// Posts component (definition)
+const useData = fetchPosts('https://fake-api.tractian.com/companies')
+
 const Posts = () => {
-  // No need for loading states
-  const posts = postsData()
+  // useEffect cannot be used,
+  // needs to be run on every render
+  // hook will run only once
+  const posts = useData()
   return (
-    <div>
+    <div className="wrapper-company">
+      <h3>Choose a company:</h3>
       {posts.map((post: any, index) => (
-        <div key={index}>{post.name}</div>
+        <a className="company" key={index} onClick={handleClick}>
+          <img className="icon" src="/icon-company.png" />
+          {post.name}
+        </a>
       ))}
     </div>
   )
@@ -15,45 +22,28 @@ const Posts = () => {
 
 export default Posts
 
-// Fetch external data
 function fetchPosts(url: string) {
   let status = 'pending'
   let result: any
 
-  // let fetching = new Promise((resolve, reject) => {
-  //   setTimeout(() => {
-  //     status = 'fulfilled'
-  //     result = 'bla'
-  //     resolve(result)
-  //   }, 3000)
-  // })
   let fetching = fetch(url)
     .then((res) => res.json())
     .then(async (res) => {
-      console.log('then')
       await sleep(3000)
       return res
     })
     .then((success) => {
-      console.log('onSuccess')
-
       status = 'fulfilled'
       result = success
     })
-    // Fetch request has failed
     .catch((error) => {
       status = 'rejected'
       result = error
     })
 
-  console.log('run fetchPosts')
-
   return () => {
-    console.log('run status:', status)
-
-    // throw fetching
     if (status === 'pending') {
-      throw fetching // Suspend(A way to tell React data is still fetching)
+      throw fetching // Suspend(a way to tell React data is still fetching)
     } else if (status === 'rejected') {
       throw result // Result is an error
     } else if (status === 'fulfilled') {
@@ -64,4 +54,8 @@ function fetchPosts(url: string) {
 
 function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+function handleClick() {
+  alert('You clicked me!')
 }
--- src/components/VisualTree/index.tsx
+++ src/components/VisualTree/index.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from 'react'
-import Posts from './Posts'
+import Posts from '../CompaniesList/Posts'
 
 const MyPosts = () => {
   return (
